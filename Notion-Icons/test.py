a90b5d9b8b8adbe8199a6a386908948d129c9af5
waaaaaaaaaaaaaaaaaaaaa <jiaruiliao@gmail.com>
2021-11-15 19:29:06 +1100

    add atom

--- Notion-Icons/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-dir = os.path.dirname(os.path.realpath(__file__))
-folder_list = next(os.walk(dir))[1]
-folder_list.remove("icons")
-folder_list.remove("iconbackup")
-print(folder_list)
